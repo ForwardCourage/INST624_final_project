ea525b0477e76ec7154028f1c3b338cb0a9e10c2
Sean Liang <seanliangage@gmail.com>
2025-12-12 21:07:03 -0500

    MeowApiCaller methods updated

--- API_reader.py
+++ API_reader.py
@@ -2,28 +2,45 @@ import requests
 import json
 
 api_url = 'https://meowfacts.herokuapp.com/'
-api_resp = requests.get(api_url)
-api_content = json.loads(api_resp.content)
-api_content['data'][0]
+# api_resp = requests.get(api_url)
+# api_content = json.loads(api_resp.content)
+# api_content['data'][0]
 class MeowApiCaller:
 
-    def __init__(self, num) -> None:
+    def __init__(self) -> None:
         self.facts = []
+        self.length = 0
+
+    def updateLen(self):
+        self.length = len(self.facts)
 
 
-    def get_facts(self, num):
-        for i in range(num):
-            resp = requests.get(api_url)
-            content = json.loads(resp.content)
-            fact = content['data'][0]
-            self.facts.append(fact)
+    def getFacts(self, num):
+        api_with_count = f'{api_url}/?count={num}'
+        resp = requests.get(api_with_count)
+        content = json.loads(resp.content)
+        self.facts = content['data']
+        self.updateLen()
+    
+    def printFacts(self, num):
 
-    def return_facts(self):
+        for i in range(min(num, self.returnLen())):
+            print(self.facts[i])
+
+
+    def returnFacts(self):
         return self.facts
+    
+    def returnLen(self):
+        return self.length
 
-    def cleanup(self):
+    def cleanUp(self):
         self.facts = []
+        self.updateLen()
 
-    def getFactsNum(self):
-        return len(self.facts)
+    
 
+if __name__ == '__main__':
+    caller = MeowApiCaller()
+    caller.getFacts(10)
+    caller.printFacts(15)
