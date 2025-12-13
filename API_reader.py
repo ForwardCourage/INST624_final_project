fb7cccd00622e65abe931a0d8d842a4288a1a393
Sean Liang <seanliangage@gmail.com>
2025-12-12 23:12:31 -0500

    Docstring updated

--- API_reader.py
+++ API_reader.py
@@ -1,21 +1,31 @@
 import requests
 import json
 
-api_url = 'https://meowfacts.herokuapp.com/'
-# api_resp = requests.get(api_url)
-# api_content = json.loads(api_resp.content)
-# api_content['data'][0]
+api_url = 'https://meowfacts.herokuapp.com/' # Meowfacts API url
+
 class MeowApiCaller:
+    """
+    This is an API caller designed to get data from Meowfacts API.
+
+    Meowfacts API is a very simple API that requires little data processing. 
+    It can return facts about cats in a simple format.
+    """
 
     def __init__(self) -> None:
         self.facts = []
         self.length = 0
 
     def updateLen(self):
+        """ 
+        This methods updates its length argument.
+        """
         self.length = len(self.facts)
 
 
     def getFacts(self, num):
+        """
+        This method gets facts of appointed numbers from Meowfacts API
+        """
         api_with_count = f'{api_url}/?count={num}'
         resp = requests.get(api_with_count)
         content = json.loads(resp.content)
@@ -23,7 +33,12 @@ class MeowApiCaller:
         self.updateLen()
     
     def printFacts(self, num):
+        """
+        This method prints out the facts stored in the API caller.
 
+        If the parameter exceeds the length of the facts list of the caller,
+        then all facts stored will be printed out.
+        """
         for i in range(min(num, self.returnLen())):
             print(self.facts[i])
 
@@ -35,6 +50,9 @@ class MeowApiCaller:
         return self.length
 
     def cleanUp(self):
+        """
+        This method cleans up all facts stored in it and update the length.
+        """
         self.facts = []
         self.updateLen()
 
