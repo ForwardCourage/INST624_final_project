48d3e9227d503b786a00d70f30ba218b7fa0dab9
Sean Liang <seanliangage@gmail.com>
2025-12-12 20:39:14 -0500

    MeowApiCaller methods defined

--- API_reader.py
+++ API_reader.py
@@ -4,13 +4,26 @@ import json
 api_url = 'https://meowfacts.herokuapp.com/'
 api_resp = requests.get(api_url)
 api_content = json.loads(api_resp.content)
-
+api_content['data'][0]
 class MeowApiCaller:
 
     def __init__(self, num) -> None:
         self.facts = []
-        self.num = num
 
-    def get_facts(self):
-        for i in range(self.num):
-            pass
+
+    def get_facts(self, num):
+        for i in range(num):
+            resp = requests.get(api_url)
+            content = json.loads(resp.content)
+            fact = content['data'][0]
+            self.facts.append(fact)
+
+    def return_facts(self):
+        return self.facts
+
+    def cleanup(self):
+        self.facts = []
+
+    def getFactsNum(self):
+        return len(self.facts)
+
